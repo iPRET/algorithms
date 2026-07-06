2435d0172f7b39fbdff32b71db358d7dc2472b19
iPRET <Ingus.Pret@gmail.com>
2026-07-06 23:10:18 +0300

    Fix off by 1 error

--- bubble_sort.py
+++ bubble_sort.py
@@ -6,7 +6,7 @@ def bubble_sort(arr: list) -> list:
   :return: New sorted list.
   """
   arr = arr.copy()
-  for i in range(len(arr) - 2):
+  for i in range(len(arr) - 1):
     for j in range(0, len(arr) - 1 - i):
       if arr[j + 1] < arr[j]:
         tmp = arr[j]
