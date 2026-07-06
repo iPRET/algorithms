8b89500f9b6804c9d75e4248abae0fecb4554ac0
iPRET <Ingus.Pret@gmail.com>
2026-07-06 23:12:01 +0300

    Change bubble_sort.py element swap to be cleaner

--- bubble_sort.py
+++ bubble_sort.py
@@ -9,9 +9,7 @@ def bubble_sort(arr: list) -> list:
   for i in range(len(arr) - 1):
     for j in range(0, len(arr) - 1 - i):
       if arr[j + 1] < arr[j]:
-        tmp = arr[j]
-        arr[j] = arr[j + 1]
-        arr[j + 1] = tmp
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
   return arr
 
 test = [5, 1, 7, 4, 3, 2, 6, 8, 9, 10]
